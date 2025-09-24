1bc55bcc44babe4e0d9d0b3b73d58b1586b4991d
Harold1Maldonado <haroldmaldonado1@gmail.com>
2025-09-24 18:37:35 +0000

    se centra el contenido usando CSS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function() {
-  // Palos y valores
+ 
   const suits = ["heart", "spade", "club", "diamond"];
   const suitSymbols = {
     heart: "♥",
@@ -11,13 +11,12 @@ window.onload = function() {
   };
   const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-  // Selección aleatoria
+ 
   let randomSuit = suits[Math.floor(Math.random() * suits.length)];
   let randomValue = values[Math.floor(Math.random() * values.length)];
 
-  // Construir carta
-  const card = document.getElementById("card");
-  card.className = `card ${randomSuit}`; // aplica estilo según palo
+  const card = document.getElementById("randomCard");
+  card.className = `card ${randomSuit}`; 
   card.innerHTML = `
     <div class="top">${suitSymbols[randomSuit]}</div>
     <div class="center">${randomValue}</div>
